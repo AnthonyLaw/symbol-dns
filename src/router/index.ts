a69ce85c17f4e4398f389d38a3e2a02307313cbf
AnthonyLaw <yc-law1015@hotmail.com>
2021-08-25 20:06:13 +0800

    feat: add Error page view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
+import Error from '@/views/Error.vue';
 
 const routes: Array<RouteRecordRaw> = [
     {
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/error',
+        name: 'Error',
+        component: Error,
     },
 ];
 
---
+++ src/views/Error.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>Opps, Something went wrong.</div>
+    <div>The domain could not be resolved by Symbol-DNS. Please try another domain...</div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    components: {},
+})
+export default class Error extends Vue {}
+</script>
