bc1a6c33d75870dcff4267443d056cede1f75879
AnthonyLaw <yc-law1015@hotmail.com>
2021-08-26 20:53:50 +0800

    feat: add display domain tld component

---
+++ src/components/DisplayDomainTLD.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="wrapper">
+        <div class="domains-holder">
+            <div v-for="(domain, index) in domains" :key="'tld_' + index" class="domains">
+                {{ domain }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+    props: {
+        domains: Array,
+    },
+})
+export default class DisplayDomainTLD extends Vue {}
+</script>
+
+<style lang="scss" scope>
+.wrapper {
+    @apply text-center text-xl;
+
+    .domains-holder {
+        @apply inline-flex py-2;
+    }
+
+    .domains {
+        @apply text-red-400 px-5;
+    }
+
+    .domains:hover {
+        @apply text-red-600 px-5;
+    }
+}
+</style>
