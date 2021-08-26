592bff1f50ab8bc032e5b9a37ca678db79b16895
AnthonyLaw <yc-law1015@hotmail.com>
2021-08-26 23:19:19 +0800

    fix: change endpoints with https

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,9 @@ import { NetworkType } from 'symbol-sdk';
 
 export const Network = {
     endpoints: [
+        'https://sym-test-01.opening-line.jp:3001',
         'https://sym-test-02.opening-line.jp:3001',
-        'http://ngl-dual-501.testnet.symboldev.network:3000',
-        'http://ngl-api-001.testnet.symboldev.network:3000',
+        'https://stg.harvesting-sweet-potatoes.club:3001',
     ],
     generationHash: '3B5E1FA6445653C971A50687E75E6D09FB30481055E3990C84B25E9222DC1155',
     metadataKey: 'D9766DB9BFCFF27C', // symbol-dns-record-1
