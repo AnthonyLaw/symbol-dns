4a64ee371a08fa67d913e84d320fb562f716a4d5
AnthonyLaw <yc-law1015@hotmail.com>
2021-08-26 20:54:34 +0800

    feat: add message on home page.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,55 @@
 <template>
-    <div>write content</div>
+    <div>
+        <div class="card">
+            <div class="title">Symbol-DNS</div>
+            <div>
+                Decentralized internet domain builds on top of symbol blockchain. Anyone can own decentralized Top-level domains (TLD)
+                Top-level domains (TLD)
+            </div>
+        </div>
+
+        <div class="card">
+            <div class="title">Top-level domains (TLD)</div>
+            <DisplayDomainTLD :domains="centralizedTLDs" />
+
+            <div><b>Centralized</b> TLD, it controlled and monitor by <b>ICANN</b>.</div>
+            <DisplayDomainTLD :domains="decentralizedTLDs" />
+            <DisplayDomainTLD :domains="decentralizedDomains" />
+
+            <div><b>Decentralized</b> TLD, it controlled and monitor <b>YOU</b>.</div>
+            <div>Create custom TLD names along with domain names and it keeps your info private.</div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
+import DisplayDomainTLD from '@/components/DisplayDomainTLD.vue';
 
 @Options({
-    components: {},
+    components: { DisplayDomainTLD },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+    get centralizedTLDs() {
+        return ['.com', '.org', '.io'];
+    }
+
+    get decentralizedTLDs() {
+        return ['.happy', '.sunday', '.zero'];
+    }
+
+    get decentralizedDomains() {
+        return ['super.happy', 'ipfs.sunday', 'nft.zero'];
+    }
+}
 </script>
+
+<style lang="scss" scoped>
+.card {
+    @apply p-3 max-w-lg;
+}
+
+.title {
+    @apply text-2xl text-left mb-2 font-extrabold;
+}
+</style>
