24d6e2f2dd610c40623ab73629737a76b5b1ce87
AnthonyLaw <yc-law1015@hotmail.com>
2021-08-28 22:17:41 +0800

    fix: metadata key in constants

--- src/constants.ts
+++ src/constants.ts
@@ -7,6 +7,7 @@ export const Network = {
         'https://stg.harvesting-sweet-potatoes.club:3001',
     ],
     generationHash: '3B5E1FA6445653C971A50687E75E6D09FB30481055E3990C84B25E9222DC1155',
-    metadataKey: 'D9766DB9BFCFF27C', // symbol-dns-record-1
+    metadataKeyHex: 'D9766DB9BFCFF27C',
+    metadataKey: 'symbol-dns-record-1',
     type: NetworkType.TEST_NET,
 };
